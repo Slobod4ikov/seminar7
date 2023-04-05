b677f2a284d3f4b85adb7c14f18650847cf80f76
Oleg <slobodchikov_1990@mail.ru>
2023-04-05 23:14:43 +0300

    solved task 46

--- Program.cs
+++ Program.cs
@@ -4,3 +4,24 @@ m = 3, n = 4.
 0,5 7 -2 -0,2
 1 -3,3 8 -9,9
 8 7,8 -7,1 9*/
+
+int m, n;
+Console.WriteLine("Введите количество строк массива");
+int.TryParse(Console.ReadLine(), out m);
+Console.WriteLine("Введите количество столбцов массива");
+int.TryParse(Console.ReadLine(), out n);
+double [,] array = new double [m,n]; 
+for (int i = 0; i < array.GetLength(0); i++){
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array [i,j]=Math.Round((new Random( ).NextDouble()*new Random().Next(-9, 10)),1);
+        
+    }
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++){
+        Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+}
